71c67b44cc81c28b45946c0514165dccf431fed7
Denis Vásquez <123614674+denisvasquez@users.noreply.github.com>
2023-05-31 14:50:13 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,72 @@
-import tocrypt
 import tkinter as tk
 from tkinter import ttk
+from tocrypt import encode, decode, pass_to_ascii
 
 class App:
     def __init__(self, master):
-        self.encryptor = tocrypt
         self.master = master
         self.master.title("Encriptador de texto") 
+        self.master.resizable(False, False)
         self.buttons()
         self.labels()
         self.inputs();
         self.table()
-        self.check_radios()
     
-    def check_radios(self):
-        self.radio1 = ttk.Radiobutton(self.master, text="Juego de llaves 1", value="1")
-        self.radio2 = ttk.Radiobutton(self.master, text="Juego de llaves 2", value="2")
-        self.radio3 = ttk.Radiobutton(self.master, text="Juego de llaves 3", value="3")
-
-        self.radio1.grid(row=0, column=0)
-        self.radio2.grid(row=0, column=1)
-        self.radio3.grid(row=0, column=2)
-
     def buttons(self):
         self.btn = ttk.Button(self.master, text="Encriptar", command=lambda: self.encrypt(
             self.text_to_encrypt.get(),
-            self.times_to_encrypt.get()
-        ))
-        self.btn.grid(row=3, column=2)
+            self.times_to_encrypt.get(),
+            self.key_selected.get()
+        ), width=50)
+        self.btn.grid(row=4, column=2, sticky="w")
     
     def inputs(self):
-        self.text_to_encrypt = ttk.Entry(self.master)
-        self.times_to_encrypt = ttk.Entry(self.master)
+        self.text_to_encrypt = ttk.Entry(self.master, width=50)
+        self.times_to_encrypt = ttk.Entry(self.master, width=50)
+        self.key_selected = ttk.Entry(self.master, width=50)
 
-        self.times_to_encrypt.grid(row=2, column=2)
-        self.text_to_encrypt.grid(row=1, column=2)
+        self.text_to_encrypt.grid(row=1, column=2, pady=6)
+        self.times_to_encrypt.grid(row=2, column=2, pady=6)
+        self.key_selected.grid(row=3, column=2, pady=6)
     
     def labels(self):
         lbl_to_encrypt = ttk.Label(self.master, text="Texto a encriptar:")
         lbl_times = ttk.Label(self.master, text="Veces a encriptar:")
+        lbl_keys = ttk.Label(self.master, text="Juego de llaves (1, 2, 3):")
 
         lbl_to_encrypt.grid(row=1, column=0)
         lbl_times.grid(row=2, column=0)
+        lbl_keys.grid(row=3, column=0)
     
-    def encrypt(self, text, times):
+    def encrypt(self, text, times, key):
         if (text == "" or times == ""):
             print("Alguno de los campos estan vacios")
             return
         
         try:
-            if (int(times) > 3 or int(times) < 1):
+            key = int(key)
+            if (int(key) > 3 or int(key) < 1):
                 print("La llave seleccionada no es valida")
                 return
+            try:
+                times = int(times)
+            except ValueError:
+                print("NO SE PUEDE INGRESAR LETRAS EN EL NUMERO DE VECES")
+                return
         except ValueError:
-            print("NO SE PUEDE INGRESAR LETRAS EN EL NUMERO DE VECES")
+            print("NO SE PUEDE INGRESAR LETRAS EN EL JUEGO DE LLAVES")
             return
 
-        print("Informacion: ", text, times)
+        encrypted = encode(pass_to_ascii(text), str(key), times)        
+        datos = {
+            "encrypted": encrypted,
+            "times": times,
+            "key": key
+        }
+        print(datos)
 
     def table(self):
         pass
-
-# def main():
-#     text = str(input("Insert the text to encript: "))
-#     key = int(input("Insert the key (1-3): ")) 
-#     times = int(input("Insert the times: "))
-#     if (key > 3 and key < 1):
-#         print("Invalid key")
-#         return
-
-#     encripted = encrypt(pass_to_ascii(text), keys[str(key)], times, 0)
-
-#     print("original: ", pass_to_ascii(text))
-#     print("encypted: ", encripted)
 
 if __name__ == "__main__":
     window = master = tk.Tk()
--- tocrypt.py
+++ tocrypt.py
@@ -1,36 +1,32 @@
 import json
 
-class to_crypt:
-    def __init__(self):
-        with open("./keys.json", "r") as f:
-            self.keys = json.load(f)
+with open("./keys.json", "r") as f:
+        keys = json.load(f)
 
-    def pass_to_ascii(self, text):
-        ascii_values = [ord(char) for char in text]
-        return ascii_values;
+def pass_to_ascii(text):
+    ascii_values = [ord(char) for char in text]
+    return ascii_values;
 
-    def encode(self, text, key, times, i = 0):
-        secuency_ascii = self.pass_to_ascii(text)
-        exp_private = self.keys[key]["exp_private"]
-        public = self.keys[key]["public"]
+def encode(array, key, times):
+    exp_private = keys[key]["exp_private"]
+    public = keys[key]["public"]
 
-        if i == times:
-            return;
+    if times == 0:
+        return array;
 
-        i = i + 1
+    array = [(item**exp_private) % public for item in array]
 
-        text_encripted = [(item**exp_private) % public for item in secuency_ascii]
-        self.encode(text_encripted, key, times, i)
+    return encode(array, key, times-1)
 
-        print(text_encripted)
+def decode(secuency_encrypted, key, times, i = 0):
+    private = keys[key]["private"]
+    public = keys[key]["public"]
 
-        return text_encripted
+    i = i + 1
 
-    def decode(self, secuency_encrypted, key):
-        private = self.keys[key]["private"]
-        public = self.keys[key]["public"]
+    text_normal = [(item**private) % public for item in secuency_encrypted]
 
-        text_normal = [(item**private) % public for item in secuency_encrypted]
+    decode(text_normal, key, times, i)
 
-        return text_normal
+    return text_normal
 
